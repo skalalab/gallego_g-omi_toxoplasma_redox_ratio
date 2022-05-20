d87f7ecc87a3d63d7aafcf67af93f9e58f260db6
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2022-05-20 14:10:23 -0500

    added experimental toxo segmentation for individual timepoints

--- processing_toxo_masks.py
+++ processing_toxo_masks.py
@@ -40,14 +40,14 @@ for path_dict_dataset in list_datasets[:1]:
             path_output = path_dataset / "generated_toxo_masks"
 
 
-    for index, row_data in list(df_dataset_toxo.iterrows())[:]:
+    for index, row_data in list(df_dataset_toxo.iterrows())[-10:]:
         pass
         im = tifffile.imread(row_data['toxo_photons']) # 
         
         
         fig, ax = plt.subplots(1,6, figsize=(14,3))
         
-        fig.suptitle(Path(row_data['toxo_photons']).name)
+        fig.suptitle(f"{Path(row_data['toxo_photons']).name} \n hours: {row_data['time_hours']}")
         
         ax[0].set_title("original")
         ax[0].imshow(im, ) # vmax=np.percentile(im,99)
@@ -73,8 +73,34 @@ for path_dict_dataset in list_datasets[:1]:
         ax[3].imshow(im_diff_gauss) #, vmax=30
         ax[3].set_axis_off()
         
-        im_kmeans = kmeans_threshold(im_diff_gauss, k=6, n_brightest_clusters=2)
-        ax[4].set_title("k-means 6 keep 2")
+        
+        # toxo gets brighter as the timecourse goes on
+        # experiment different k values for different 
+        # timepoints
+        # if row_data['time_hours'] == 1:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 3:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 6:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 9:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 12:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 24:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 48:
+        #     k = 6 ; n_brightest_clusters=2
+        # elif row_data['time_hours'] == 72:
+        #     k = 6 ; n_brightest_clusters=2
+            
+        k = 6 ; n_brightest_clusters=2
+            
+            
+        im_kmeans = kmeans_threshold(im_diff_gauss, 
+                                     k=k, 
+                                     n_brightest_clusters=n_brightest_clusters)
+        ax[4].set_title(f"k-means {k} keep {n_brightest_clusters}")
         ax[4].imshow(im_kmeans)
         ax[4].set_axis_off()
         
